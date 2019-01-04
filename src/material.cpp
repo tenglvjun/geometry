cc96663eef67c539209f101a359f82735fd55596
Tony <tenglvjun@gmail.com>
2019-01-04 10:45:00 +0800

    add material

---
+++ src/material.cpp
@@ -0,0 +1,91 @@
+#include "material.h"
+
+
+GeoMaterial::GeoMaterial(const GeoVector3D& ambient, const GeoVector3D& diffuse, const GeoVector3D& specular, const double shininess)
+    : m_ambient(ambient), m_diffuse(diffuse), m_specular(specular), m_shininess(shininess)
+{
+
+}
+
+GeoMaterial::GeoMaterial(const GeoMaterial& material)
+{
+    m_ambient = material.m_ambient;
+    m_diffuse = material.m_diffuse;
+    m_specular = material.m_specular;    
+    m_shininess = material.m_shininess;
+}
+
+GeoMaterial::~GeoMaterial()
+{
+
+}
+
+GeoMaterial& GeoMaterial::operator=(const GeoMaterial& material)
+{
+    if (&material == this) {
+        return *this;
+    }
+
+    m_ambient = material.m_ambient;
+    m_diffuse = material.m_diffuse;
+    m_specular = material.m_specular;    
+    m_shininess = material.m_shininess;
+
+    return *this;
+    
+}
+
+GeoVector3D GeoMaterial::Ambient() const
+{
+    return m_ambient;
+}
+
+void GeoMaterial::Ambient(const GeoVector3D &ambient)
+{
+    m_ambient = ambient;
+}
+
+GeoVector3D GeoMaterial::Specular() const
+{
+    return m_specular;
+}
+
+void GeoMaterial::Specular(const GeoVector3D &specular)
+{
+    m_specular = specular;
+}
+
+GeoVector3D GeoMaterial::Diffuse() const
+{
+    return m_diffuse;
+}
+
+void GeoMaterial::Diffuse(const GeoVector3D &diffuse)
+{
+    m_diffuse = diffuse;
+}
+
+double GeoMaterial::Shininess() const
+{
+    return m_shininess;
+}
+
+void GeoMaterial::Shininess(const double &shininess)
+{
+    m_shininess = shininess;
+}
+
+void GeoMaterial::ApplyShader(const Shader &shader) const
+{
+    std::vector<float> value;
+
+    m_ambient.Flatten(value);
+    shader.SetVector("material.ambient", 3, &value[0]);
+    value.clear();
+    m_diffuse.Flatten(value);
+    shader.SetVector("material.diffuse", 3, &value[0]);
+    value.clear();
+    m_specular.Flatten(value);
+    shader.SetVector("material.specular", 3, &value[0]);
+    shader.SetFloat("material.shininess", (float)m_shininess);
+}
---
+++ src/material.h
@@ -0,0 +1,42 @@
+#if !defined(__MATERIAL_HEAD_FILE__)
+#define __MATERIAL_HEAD_FILE__
+
+#include "vector.h"
+#include "shader.h"
+
+class GeoMaterial
+{
+public:
+    GeoMaterial(const GeoVector3D& ambient, const GeoVector3D& diffuse, const GeoVector3D& specular, const double shininess);
+    GeoMaterial(const GeoMaterial& material);
+    virtual ~GeoMaterial();
+
+public:
+    GeoMaterial& operator=(const GeoMaterial& material);
+
+public:
+    GeoVector3D Ambient() const;
+    void Ambient(const GeoVector3D &ambient);
+
+    GeoVector3D Specular() const;
+    void Specular(const GeoVector3D &specular);
+
+    GeoVector3D Diffuse() const;
+    void Diffuse(const GeoVector3D &diffuse);
+
+    double Shininess() const;
+    void Shininess(const double &shininess);
+
+    void ApplyShader(const Shader &shader) const;
+
+private:
+    GeoMaterial();
+
+protected:
+    GeoVector3D m_ambient;
+    GeoVector3D m_diffuse;
+    GeoVector3D m_specular;    
+    double m_shininess;
+};
+
+#endif // __MATERIAL_HEAD_FILE__
