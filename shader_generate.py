784e52d8adee46a749ddcf7c8d14e96f1feb7d5a
Tony Teng <tenglvjun@gmail.com>
2019-01-19 09:37:28 +0800

    refactoring

--- shader_generate.py
+++ shader_generate.py
@@ -1,27 +1,39 @@
 import os
 import shutil
-
-current_path = os.path.dirname(__file__)
-os.chdir(current_path)
+import sys
 
 shader_dir = 'shader'
 bin_dir = 'bin/shader'
-
-list = os.listdir(shader_dir)
-
 include_path = shader_dir+os.path.sep+'include'
-
-for i in range(0, len(list)):
-    path = os.path.join(shader_dir, list[i])
-    if os.path.isfile(path):
-        input_file = path
-        output_file = path.replace('.m4', '')
-        os.system('m4 -I ' + include_path + ' ' +
-                  input_file + ' > ' + output_file)
 
 isExists = os.path.exists(bin_dir)
 if not isExists:
     os.makedirs(bin_dir)
 
-os.system('mv ' + shader_dir + os.path.sep+'*.vs ' + bin_dir)
-os.system('mv ' + shader_dir + os.path.sep+'*.fs ' + bin_dir)
+def RemoveShaderFiles(path):
+    list = os.listdir(path)
+    for i in range(0, len(list)):
+        ext = os.path.splitext(path)
+        if (ext == ".vs") or (ext == ".fs"):
+            os.remove(os.path.join(path, list[i]))
+
+def GenerateShaderFiles(path):
+    shader_list = []
+    list = os.listdir(path)
+    for i in range(0, len(list)):
+        path = os.path.join(shader_dir, list[i])
+        if os.path.isfile(path):
+            input_file = path
+            output_file = path.replace('.m4', '')
+            os.system('m4 -I ' + include_path + ' ' +
+                    input_file + ' > ' + output_file)
+            shader_list.append(output_file)
+
+    for i in range(0, len(shader_list)):
+        shutil.move(shader_list[i], bin_dir)
+
+
+
+RemoveShaderFiles(shader_dir)
+GenerateShaderFiles(shader_dir);
+
